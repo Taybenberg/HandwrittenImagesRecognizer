8c101b8d3b01a11a502499470d49534bd3e008cc
Вдовенко Віталій <taybenberg@gmail.com>
2019-07-12 14:55:30 +0300

    Dataset path fix

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            new Teacher(@"C:\Users\taybe\source\repos\numbers");
-            //new Teacher().Recoznize(@"C:\Users\taybe\source\repos\numbers\0\number-0000000.PNG");
+            new Teacher(yourDatasetPath);
+            new Teacher().Recoznize(yourImagePath);
         }
     }
 }
